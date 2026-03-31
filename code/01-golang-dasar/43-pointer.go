aaddbbc21c74fe0ae4466f36952ef8457b0cc8f0
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-03-31 06:52:37 +0000

    feat: pointer

---
+++ code/01-golang-dasar/43-pointer.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	address1 := Address{"Subang", "Jawa Barat", "Indonesia"}
+	address2 := &address1 // Pointer ke address 1
+
+	address2.City = "Bandung" // mengubah address 1
+
+	fmt.Println(address1)
+	fmt.Println(address2)
+}
