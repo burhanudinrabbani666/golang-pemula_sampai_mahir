02a749c23b8307d39447b25385506033c74b1752
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-04-02 20:40:13 +0700

    feat: menggagalkan testing

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,13 +1,42 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T){
 	result := HelloWorld("Bani")
 
 	if result != "Hello Bani"{
 		// unit test failed
-		panic("Result is not Hello Bani")
+		t.Fail()
 	}
+
+	fmt.Println("Ini test Hello World")
 }
 
+func TestHelloWorldBani(t *testing.T){
+	result := HelloWorld("Bani")
+
+	if result != "Hello Bani"{
+		// unit test failed
+		t.Error("Result must be Hello Bani") // Ini akan lanjut eksekusi line berikutnya
+	}
+
+	fmt.Println("Dieksekusi walaupun Error")
+
+}
+
+
+func TestHelloWorldUdin(t *testing.T){
+	result := HelloWorld("Bani")
+
+	if result != "Hello Bani"{
+		// unit test failed
+		t.Fatal("Result must be Hello Bani") // Ini tidak akan lanjut eksekusi line berikutnya
+	}
+
+	fmt.Println("Tidak dieksekusi ketika Error")
+
+}
